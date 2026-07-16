45be6f7c1f8edd5956cf0f7fab4c7d98f72e22c4
Khumoetsile Phatudi <kdphatudi88@gmail.com>
2026-07-16 13:41:53 +0200

    Complete lesson 1

--- lesson1.py
+++ lesson1.py
@@ -3,3 +3,5 @@ name = input("Enter your name: ")
 artist = input("Which band or artist would you like to see? ")
 
 print(f"/nHey {name}! Your tickets to see {artist} are booked successfully!")
+
+# Lesson 1 completed
