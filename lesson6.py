f8b088ae485e733019b7947ed50e8a787b69b8d2
Khumoetsile Phatudi <kdphatudi88@gmail.com>
2026-07-16 18:38:09 +0200

    Complete lesson6

---
+++ lesson6.py
@@ -0,0 +1,21 @@
+# Working List (Shopping Cart)
+
+cart = ["Apples", "Eggs", "Milk"]
+print(cart[0])
+
+cart.append("Bread")
+print(cart)
+
+for item in cart:
+    print(item)
+
+# Working with dictionaries
+
+student = {
+    "name": "Khumo",
+    "age": 37,
+    "course": "Intro to Python"
+}
+
+print(student["name"])
+print(student["age"])
